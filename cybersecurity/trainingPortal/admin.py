41bd3de65a8fb8787fba554686fe7a19e63e02c3
IoannisCleary <nicolemaniac@gmail.com>
2015-02-08 19:22:18 +0000

    Django Quiz shortcuts added to the CMS
    Create Category,SubCategory and create/view ,Quizzes and Question types

--- cybersecurity/trainingPortal/admin.py
+++ cybersecurity/trainingPortal/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from trainingPortal.models import Page,Chapter,Profile
 
+
 # Register your models here.
 admin.site.register(Chapter)
 admin.site.register(Profile)
--- cybersecurity/trainingPortal/cms_toolbar.py
+++ cybersecurity/trainingPortal/cms_toolbar.py
@@ -30,3 +30,33 @@ class PageToolbar(CMSToolbar):
         menu.add_sideframe_item(_('View Pages'), url=url2)
         menu.add_sideframe_item(_('Create Page'), url=url)
         admin_menu.add_break('pages-break', position=menu)
+@toolbar_pool.register
+class QuizToolbar(CMSToolbar):
+
+    def populate(self):
+        admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER, _('Training Portal'))
+        position = admin_menu.find_first(Break, identifier=ADMINISTRATION_BREAK)
+        menu = admin_menu.get_or_create_menu('add_quiz', _('Quiz'), position=position)
+        url = reverse('admin:quiz_category_add')
+        menu.add_sideframe_item(_('Create Category'), url=url)
+        url3 = reverse('admin:quiz_subcategory_add')
+        menu.add_sideframe_item(_('Create SubCategory'), url=url3)
+        url2 = reverse('admin:quiz_quiz_add')
+        url4 = reverse('admin:quiz_quiz_changelist')
+        menu.add_sideframe_item(_('Create Quiz'), url=url2)
+        menu.add_sideframe_item(_('View Quizzes'), url=url4)
+        admin_menu.add_break('quiz-break', position=menu)
+@toolbar_pool.register
+class QuestionsToolbar(CMSToolbar):
+
+    def populate(self):
+        admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER, _('Training Portal'))
+        position = admin_menu.find_first(Break, identifier=ADMINISTRATION_BREAK)
+        menu = admin_menu.get_or_create_menu('add_question', _('Questions'), position=position)
+        menu.add_sideframe_item(_('Create MultipleChoiceType'), url="/admin/multichoice/mcquestion/add/")
+        menu.add_sideframe_item(_('View MultipleChoiceType'), url="/admin/multichoice/mcquestion/")
+        menu.add_sideframe_item(_('Create True/FalseType'), url="/admin/true_false/tf_question/add/")
+        menu.add_sideframe_item(_('View True/FalseType'), url="/admin/true_false/tf_question/")
+        menu.add_sideframe_item(_('Create EssayType'), url="/admin/essay/essay_question/add/")
+        menu.add_sideframe_item(_('View EssayType'), url="/admin/essay/essay_question/")
+        admin_menu.add_break('question-break', position=menu)
