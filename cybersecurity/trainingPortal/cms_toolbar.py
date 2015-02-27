43219f142fb46642c2fc37d75a7ee83e8741fb57
IoannisCleary <nicolemaniac@gmail.com>
2015-02-27 16:31:51 +0000

    Django File added to cms toolbar
    Help information on how to upload a file and add a link to it - added

--- cybersecurity/trainingPortal/cms_toolbar.py
+++ cybersecurity/trainingPortal/cms_toolbar.py
@@ -16,7 +16,15 @@ class ProfileToolbar(CMSToolbar):
         url = reverse('admin:trainingPortal_profile_add')
         menu.add_sideframe_item(_('View Profiles'), url=url2)
         menu.add_sideframe_item(_('Create Profile'), url=url)
-		
+@toolbar_pool.register
+class DjangoFilerToolbar(CMSToolbar):
+
+    def populate(self):
+        admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER, _('Training Portal'))
+        position = admin_menu.find_first(Break, identifier=ADMINISTRATION_BREAK)
+        menu = admin_menu.get_or_create_menu('add_file', _('Files'), position=position)
+        menu.add_sideframe_item(_('Upload/View Files'), url="/admin/filer/folder/")
+        admin_menu.add_break('file-break', position=menu)
 @toolbar_pool.register
 class ChapterToolbar(CMSToolbar):
 
@@ -29,7 +37,7 @@ class ChapterToolbar(CMSToolbar):
         menu.add_sideframe_item(_('View Chapters'), url=url2)
         menu.add_sideframe_item(_('Create Chapter'), url=url)
         admin_menu.add_break('chapters-break', position=menu)
-		
+
 @toolbar_pool.register
 class PageToolbar(CMSToolbar):
 
